d6c326f0a766040c2185a9be68d376ed041488d0
Laura Zaguini <johnknoxx1@gmail.com>
2021-06-20 23:27:51 -0300

    Delete cl_hidehud.lua

--- lua/cda/cl_hidehud.lua
+++
@@ -1,9 +0,0 @@
-local hide = {
-	["CHudHealth"] = true,
-	["CHudBattery"] = true,
-}
-hook.Add("HUDShouldDraw", "MyAddonHUDPaint", function(name)
-	if hide[name] and IsValid(LocalPlayer()) and LocalPlayer():Health() >= 100 then
-		return false
-	end
-end)
